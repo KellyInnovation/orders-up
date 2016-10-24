50aac10e8bf09ff46963427da4d780ac44c7f623
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-10-24 15:17:09 -0400

    Add kitchen food with categories.

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
 class Kitchen(models.Model):
+	CATEGORY_CHOICES = (
+		('APPETIZERS', 'Appetizers'),
+		('SALADS', 'Salads and Soups'),
+		('BEEF', 'Beef'),
+		('SEAFOOD', 'Seafood'),
+		('POULTRY', 'Poultry'),
+		('PORK', 'Pork'),
+		('PASTA', 'Pastas'), 
+		('DRINKS', 'Drinks'),
+		('SIDES', 'Side Items'),
+		('DESSERTS', 'Desserts'),
+		('OTHER', 'Other'),
+	)
+
 
 	menu_item_name = models.CharField(max_length=200)
 	menu_item_description = models.TextField(null=True, blank=True)
 	menu_item_price = models.DecimalField(max_digits=10000, decimal_places=2)
 
 	meat_size = models.CharField(max_length=20, null=True, blank=True)
+	menu_category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='Other', null=True, blank=True)
+
--- kitchen/serializers.py
+++ kitchen/serializers.py
@@ -6,4 +6,4 @@ class KitchenSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Kitchen
-		fields = ('id', 'menu_item_name', 'menu_item_description', 'menu_item_price', 'meat_size',)
+		fields = ('id', 'menu_item_name', 'menu_item_description', 'menu_item_price', 'meat_size', 'menu_category',)
