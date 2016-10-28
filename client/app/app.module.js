2d0ce529c4b36db9d0026ef6300a012808d20b8b
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-10-28 15:00:10 -0400

    Add party angular, but other pages not showing.

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-
+import uiRouter from 'angular-ui-router';
 
 import GuestModule from '../guest/guest.module';
 import HostessModule from '../hostess/hostess.module';
@@ -9,12 +9,28 @@ import PartyModule from '../party/party.module';
 import appComponent from './app.component';
 
 const AppModule = angular.module('app', [
-
+	uiRouter,
 	GuestModule.name,
 	HostessModule.name,
 	KitchenModule.name,
 	PartyModule.name,
 ])
-    .component('app', appComponent);
+    .component('app', appComponent)
+    .config(($stateProvider, $urlRouterProvider) => {
+    	$urlRouterProvider.otherwise('/');
+
+    	$stateProvider.state('index', {
+    		url: '/',
+    		resolve: {
+    			party(partyAPIService) {
+    				return partyAPIService.getAllparties();
+    			},
+    		},
+    		component: 'partyList',
+    	}).state('party', {
+    		url: '/party/{partyId}',
+    	})
+    })
+    ;
 
 export default AppModule;
