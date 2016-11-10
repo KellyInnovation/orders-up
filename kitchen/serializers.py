423d1a83ec28dc629cdebb197a4b39fd12bfe3fa
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-11-10 08:53:21 -0500

    Remove order from Kitchen Serializer

--- kitchen/serializers.py
+++ kitchen/serializers.py
@@ -6,4 +6,5 @@ class KitchenSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Kitchen
+		#fields = ('id', 'item_name', 'item_description', 'item_price', 'meat_size', 'category', 'orders',)
 		fields = ('id', 'item_name', 'item_description', 'item_price', 'meat_size', 'category', 'orders',)
